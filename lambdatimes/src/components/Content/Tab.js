b44965fe2a0bf85d6a80fcb7282c6ab49d2ae0d0
Nicholas Pung <nicholaspung>
2019-06-07 08:40:49 -0700

    added code to make tab content show and functional

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -4,12 +4,21 @@ const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+  let classHolder = '';
+  if (props.tab === props.selectedTab) {
+    classHolder = 'tab active-tab';
+  } else {
+    classHolder = 'tab';
+  }
+
   return (
     <div
-      className={''}
+      className={classHolder}
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
+         props.selectTabHandler(props.tab)
       }}
     >
       {props.tab.toUpperCase()}
