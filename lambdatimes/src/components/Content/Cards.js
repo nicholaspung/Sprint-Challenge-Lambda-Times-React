5d894c2b81fdc724162ea461d0f3042286de6cc0
Nicholas Pung <nicholaspung>
2019-06-07 08:59:22 -0700

    added proptypes

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
 
@@ -7,19 +7,14 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
-      {props.cards.map(card => <Card card={card} />)}
+      {props.cards.map(card => <Card card={card} key={card.headline} />)}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
 Cards.propTypes = {
-  card: PropTypes.objectOf({
-    headline: PropTypes.string,
-    tab: PropTypes.string,
-    img: PropTypes.string,
-    author: PropTypes.string
-  })
+  cards: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default Cards;
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
@@ -18,7 +19,7 @@ const Tab = props => {
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
-         props.selectTabHandler(props.tab)
+        props.selectTabHandler(props.tab)
       }}
     >
       {props.tab.toUpperCase()}
@@ -27,5 +28,8 @@ const Tab = props => {
 };
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string
+}
 
 export default Tab;
